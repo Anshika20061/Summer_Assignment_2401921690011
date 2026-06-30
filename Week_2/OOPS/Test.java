1f948cafb8b7ce4b4830548f0a671ff0ea6212ee
Anshika20061 <anshikasinghmail123321@gmail.com>
2026-06-30 23:01:47 +0530

    Add files via upload

---
+++ Week_2/OOPS/Test.java
@@ -0,0 +1,42 @@
+interface Playable {
+    void play();
+}
+
+class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
+
+class Saxophone implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
+}
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // a. Create an instance of Veena and call play()
+        Veena veena = new Veena();
+        veena.play();
+
+        // b. Create an instance of Saxophone and call play()
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // c. Using interface reference (Polymorphism)
+        Playable p;
+
+        p = new Veena();
+        p.play();
+
+        p = new Saxophone();
+        p.play();
+    }
+}
